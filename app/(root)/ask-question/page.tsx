c691c0bb00c93e8b246e4936f0661be8e9d390bc
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-07-21 02:40:30 +0530

    fix clerk related and other issues

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,12 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-import { useAuth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
   // const userId = "clerk12345";
-  const { userId } = useAuth();
+  const { userId } = auth();
   if (!userId) redirect("/sign-in");
   const mongoUser = await getUserById({ userId });
   // console.log("user", mongoUser);
