3c45392ad75c5616051a5162d353ce1313f8595a
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-07-21 01:40:41 +0530

    update questionschema validation

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 const QuestionsSchema = z.object({
-  title: z.string().min(5).max(130),
+  title: z.string().min(5).max(150),
   explaination: z.string().min(100),
   tags: z.array(z.string().min(1).max(15)).min(1).max(5),
 });
